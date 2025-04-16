aeecc33fb13d22308eee311e874f46da2291cca2
victorreis7 <122209938+victorreis7@users.noreply.github.com>
2025-04-16 11:37:39 +0300

    Update Configuration.java

--- pack/src/com/greenanimalsbank/Configuration.java
+++ pack/src/com/greenanimalsbank/Configuration.java
@@ -5,4 +5,6 @@ public class Configuration {
     public static final String DB_USER = "greenadmin";
     public static final String DB_PASSWORD = "green@1234";
     public static final String API_KEY = "AIzaSyD_FAKEKEY1234567890";
+    public static final String secret="victorscrt854"
 }
+
